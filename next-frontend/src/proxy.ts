fb2e358338be624140e9754902e9176997a76034
Guilherme Cintra <gui7lk@gmail.com>
2026-02-09 07:37:34 -0300

    feat: add commitizen to the project and rename next middleware.ts to proxy.ts
    the change on the middleware happened due to the update of the file on the new version of the nextjs

--- next-frontend/src/proxy.ts
+++ next-frontend/src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const apiKey = request.cookies.get("apiKey")?.value;
 
   if (!apiKey) {
